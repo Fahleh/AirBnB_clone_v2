09c1731e2c6a2cfa61efd76c07afbad635b5ef35
Eddy <eddyukpong@gmail.com>
2024-04-04 04:48:14 -0700

    Fixes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,34 +4,59 @@
     using the function do_deploy
 """
 import os
-from fabric.api import *
+from fabric.api import env, local, put, run, runs_once
+from datetime import datetime
 
 
 env.hosts = ["100.26.243.119", "34.203.38.86"]
 env.user = "ubuntu"
 
+def do_deploy(archive_path):
+    """
+        Distributes an archive to a web server.
+    """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    time = datetime.now()
+    result = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        time.year,
+        time.month,
+        time.day,
+        time.hour,
+        time.minute,
+        time.second
+    )
+    try:
+        print("Packing web_static to {}".format(result))
+        local("tar -cvzf {} web_static".format(result))
+        size = os.stat(result).st_size
+        print("web_static packed: {} -> {} Bytes".format(result, size))
+    except Exception:
+        result = None
+    return result
+
 
 def do_deploy(archive_path):
-    """ Deploys archive to servers"""
+    """
+        Deploys the files to the host servers.
+    """
     if not os.path.exists(archive_path):
         return False
-
-    output = []
-
-    response = put(archive_path, "/tmp")
-    output.append(response.succeeded)
-
-    base_name = os.path.base_name(archive_path)
-    if base_name[-4:] == ".tgz":
-        name = base_name[:-4]
-    newdir = "/data/web_static/releases/" + name
-    run("mkdir -p " + newdir)
-    run("tar -xzf /tmp/" + base_name + " -C " + newdir)
-
-    run("rm /tmp/" + base_name)
-    run("mv " + newdir + "/web_static/* " + newdir)
-    run("rm -rf " + newdir + "/web_static")
-    run("rm -rf /data/web_static/current")
-    run("ln -s " + newdir + " /data/web_static/current")
-
-    return True
+    fd = os.path.basename(archive_path)
+    dir_name = fd.replace(".tgz", "")
+    dir_path = "/data/web_static/releases/{}/".format(dir_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(fd))
+        run("mkdir -p {}".format(dir_path))
+        run("tar -xzf /tmp/{} -C {}".format(fd, dir_path))
+        run("rm -rf /tmp/{}".format(fd))
+        run("mv {}web_static/* {}".format(dir_path, dir_path))
+        run("rm -rf {}web_static".format(dir_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(dir_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
